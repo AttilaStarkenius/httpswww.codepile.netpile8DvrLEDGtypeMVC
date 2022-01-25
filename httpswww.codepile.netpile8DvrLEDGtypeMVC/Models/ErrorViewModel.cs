66d5f137467306712880c133bbc346a290cc988e
Attila Starkenius <symphtom@hotmail.com>
2022-01-25 22:51:29 +0100

    Add project files.

---
+++ httpswww.codepile.netpile8DvrLEDGtypeMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace httpswww.codepile.netpile8DvrLEDGtypeMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
